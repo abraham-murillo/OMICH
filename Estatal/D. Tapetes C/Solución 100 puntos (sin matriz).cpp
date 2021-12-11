4d448c3978118289c6b5221b81ffe82a460502c7
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2021-12-10 22:55:52 -0600

    D. tapetes C - Solución sin matriz

---
+++ Estatal/D. Tapetes C/Solución 100 puntos (sin matriz).cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0), cout.tie(0);
+
+  int n;
+  cin >> n;
+
+  int lim = 2 * n - 1;
+
+  for (int i = 1; i <= lim; i++) {
+    for (int j = 1; j <= lim; j++) {
+      int paraArriba = i;
+      int paraAbajo = lim - i + 1;
+      int paraIzquierda = j;
+      int paraDerecha = lim - j + 1;
+      int mejorIzquierdaDerecha = min(paraIzquierda, paraDerecha);
+      int mejorArribaAbajo = min(paraArriba, paraAbajo);
+      int tipo = min(mejorIzquierdaDerecha, mejorArribaAbajo);
+      cout << n - tipo + 1 << " ";
+    }
+    cout << '\n';
+  }
+
+  return 0;
+}
