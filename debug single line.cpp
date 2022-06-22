cf5f18f7dca2c4bfc512ecb694f32411464df622
Abraham Baltazar Murillo Sandoval <36650502+abraham-murillo@users.noreply.github.com>
2022-06-22 08:09:31 -0500

    Create debug single line.cpp

---
+++ debug single line.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(x) cout << #x": " << x << endl;
+
+int main() {
+  int x = 5;
+  debug(x);
+  
+  string s = "OMICH";
+  debug(s);
+  
+  return 0;
+}
